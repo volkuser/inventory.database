f97a12e8581a82ded0c02c857a11a5a044ef66d8
volkuser <demidov008h@gmail.com>
2023-02-11 17:31:42 +0300

    control commands, data dump, tables, users

---
+++ data_dump.sql
@@ -0,0 +1,42 @@
+-- Insert data into equipment_type table
+INSERT INTO equipment_type (equipment_type_name) VALUES ('Персональный компьютер');
+INSERT INTO equipment_type (equipment_type_name) VALUES ('Проектор');
+INSERT INTO equipment_type (equipment_type_name) VALUES ('Принтер');
+
+-- Insert data into user table
+INSERT INTO "user" (username, password) VALUES ('иванов.иван', '1123');
+INSERT INTO "user" (username, password) VALUES ('петров.петр', '2123');
+INSERT INTO "user" (username, password) VALUES ('артемов.артем', '3123');
+
+-- Insert data into employee table
+INSERT INTO employee (first_name, last_name, patronymic, view_position, role, user_id)
+VALUES ('Иван', 'Иванов', 'Иввнович', 'Преподаватель', 'Responsible person', 1);
+INSERT INTO employee (first_name, last_name, patronymic, view_position, role, user_id)
+VALUES ('Петр', 'Петров', 'Петрович', 'Преподаватель', 'Administrator', 2);
+INSERT INTO employee (first_name, last_name, patronymic, view_position, role, user_id)
+VALUES ('Артем', 'Артемов', 'Артемович', 'Преподаватель', 'Commission member', 3);
+
+-- Insert data into training_center table
+INSERT INTO training_center (center_address) VALUES ('Нежинская улица, 7, Москва, 119501');
+INSERT INTO training_center (center_address) VALUES ('Нахимовский просп., 21, Москва, 117638');
+
+-- Insert data into location table
+INSERT INTO location (location_number, training_center_id) VALUES ('Кабинет 248', 1);
+INSERT INTO location (location_number, training_center_id) VALUES ('Кабинет 102', 1);
+
+-- Insert data into equipment table
+INSERT INTO equipment (model_name, product_code, description, equipment_type_id, manufacturer, cost, warranty_start, warranty_end)
+VALUES ('MacBook Pro', 'ABC123', 'MacBook Pro 16 inch', 1, 'Apple', 1999.99, '2022-01-01', '2023-01-01');
+INSERT INTO equipment (model_name, product_code, description, equipment_type_id, manufacturer, cost, warranty_start, warranty_end)
+VALUES ('Epson Projector', 'DEF456', 'Epson PowerLite 1795F', 2, 'Epson', 799.99, '2022-01-01', '2024-01-01');
+
+-- Insert data into equipment_status table
+INSERT INTO equipment_status (equipment_status_id, status) VALUES (1, 'На месте');
+INSERT INTO equipment_status (equipment_status_id, status) VALUES (2, 'Нет на месте');
+INSERT INTO equipment_status (equipment_status_id, status) VALUES (3, 'В другом месте');
+
+-- Insert data into equipment_unit table
+INSERT INTO equipment_unit (equipment_id, serial_number, purchase_date, location_id, current_status, responsible_person_id)
+VALUES (1, '123456', '2022-01-01', 1, 1, 1);
+INSERT INTO equipment_unit (equipment_id, serial_number, purchase_date, location_id, current_status, responsible_person_id)
+VALUES (2, '654321', '2022-01-01', 2, 2, 2);
---
+++ tables.sql
@@ -0,0 +1,63 @@
+CREATE TABLE "equipment_type" (
+  equipment_type_id serial PRIMARY KEY,
+  equipment_type_name varchar(50) NOT NULL
+);
+
+CREATE TABLE "user" (
+  user_id serial PRIMARY KEY,
+  username varchar(50) NOT NULL,
+  password varchar(100) NOT NULL
+);
+
+CREATE TABLE "employee" (
+  employee_id serial PRIMARY KEY,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  patronymic varchar(50) NOT NULL,
+  view_position varchar(100) NOT NULL,
+  role varchar(50) NOT NULL,
+  user_id integer NOT NULL REFERENCES "user" (user_id),
+  UNIQUE (user_id)
+);
+
+CREATE TABLE "training_center" (
+  training_center_id serial PRIMARY KEY,
+  center_address text NOT NULL
+);
+
+CREATE TABLE "location" (
+  location_id serial PRIMARY KEY,
+  location_number varchar(50) NOT NULL,
+  training_center_id integer NOT NULL REFERENCES "training_center" (training_center_id)
+);
+
+CREATE TABLE "equipment" (
+  equipment_id serial PRIMARY KEY,
+  model_name varchar(100) NOT NULL,
+  product_code varchar(50),
+  description text,
+  equipment_type_id integer REFERENCES "equipment_type"(equipment_type_id),
+  manufacturer varchar(50),
+  cost numeric(10, 2),
+  warranty_start date,
+  warranty_end date,
+  CHECK (cost >= 0),
+  CHECK (warranty_start <= warranty_end)
+);
+
+CREATE TABLE "equipment_status" (
+  equipment_status_id serial PRIMARY KEY,
+  status varchar(50) NOT NULL,
+  CHECK (status IN ('На месте', 'Нет на месте', 'В другом месте'))
+);
+
+CREATE TABLE "equipment_unit" (
+  equipment_unit_id serial PRIMARY KEY,
+  equipment_id integer REFERENCES "equipment"(equipment_id),
+  serial_number varchar(50),
+  purchase_date date,
+  location_id integer REFERENCES "location"(location_id),
+  current_status integer REFERENCES "equipment_status"(equipment_status_id),
+  responsible_person_id integer REFERENCES "employee"(employee_id),
+  guid_code uuid DEFAULT uuid_generate_v4() NOT NULL
+);
